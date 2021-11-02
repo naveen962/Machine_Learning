7a729a159fbdbc9ecc3d6d2a20fb0090445c5c41
naveen962 <75928781+naveen962@users.noreply.github.com>
2021-11-01 22:43:44 -0700

    Python program to remove a key from a dictionary

---
+++ PythonDataStructure/Dictionary/RemoveDictItems.py
@@ -0,0 +1,7 @@
+'''Write a Python program to remove a key from a dictionary.'''
+
+
+d = {1:"Ajay",2:"Abhishek",3:"Arun"} #dictionary created
+print("Original Dictionary : \n",d )
+del d[2] #delete item based on key
+print(d)
