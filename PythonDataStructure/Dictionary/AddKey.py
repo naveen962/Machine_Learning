86bd2260239f0459df0b4b1eb450eb45c5be4082
naveen962 <75928781+naveen962@users.noreply.github.com>
2021-11-01 21:59:20 -0700

    Added: Python script to add a key to a dictionary

---
+++ PythonDataStructure/Dictionary/AddKey.py
@@ -0,0 +1,8 @@
+
+'''Write a Python script to add a key to a dictionary.'''
+
+dictionary = {1:"Ajay",2:"Abhishek",3:"Arun"} #dictionary created
+print("Original Dictionary\n",dictionary)
+dictionary[4]="Sachin"                         #adding new item
+print("After adding new key to a Dictionary")
+print(dictionary)
