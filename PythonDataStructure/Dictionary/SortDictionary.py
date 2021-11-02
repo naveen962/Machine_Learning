2df9e6df5f42c88ff4e60222e8af86816d4e249b
naveen962 <75928781+naveen962@users.noreply.github.com>
2021-11-01 22:22:03 -0700

    Python script to sort (ascending and descending) a dictionary by value

--- PythonDataStructure/Dictionary/SortDictionary.py
+++ PythonDataStructure/Dictionary/SortDictionary.py
@@ -1,12 +1,12 @@
 '''Write a Python script to sort (ascending and descending) a dictionary by
-   value.'''
+   value. '''
 
 import operator
 def Ascending(d):
     return dict(sorted(d.items(), key = operator.itemgetter(1)))
 def Descending(d, reverse = False):
     return dict(sorted(d.items(), key = operator.itemgetter(1),reverse=True))
-print("dictionary elements:")
+print("dictionary elements : ")
 colors = {'Red': 1, 'Green': 3, 'Black': 5, 'White': 2, 'Pink': 4}
 print(colors)
 print("\nSorted in Ascending Order by Value :")
