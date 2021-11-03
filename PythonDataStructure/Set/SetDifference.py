1fb8c37c339951f4c2db5aab86369e5f040b3dd7
naveen962 <75928781+naveen962@users.noreply.github.com>
2021-11-03 01:40:48 -0700

    Added: Python program to create set difference

---
+++ PythonDataStructure/Set/SetDifference.py
@@ -0,0 +1,9 @@
+'''Write a Python program to create set difference.'''
+s1 = set(["green", "blue"])             #set created
+s2 = set(["blue", "yellow"])
+print("Original set elements:")
+print(s1)
+print(s2)
+print("\nDifference of two said sets:")
+s3 = s1 - s2                           #difference of two sets
+print(s3)
