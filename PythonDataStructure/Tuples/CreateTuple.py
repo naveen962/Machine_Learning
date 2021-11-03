0e5c7caad3680fab98907babb788f450cb01de77
naveen962 <75928781+naveen962@users.noreply.github.com>
2021-11-02 21:26:00 -0700

    Added: Creating a Tuple

---
+++ PythonDataStructure/Tuples/CreateTuple.py
@@ -0,0 +1,9 @@
+''' Write a Python program to create a tuple.'''
+
+tup=(10,20,30,40)  #tuple created
+print(type(tup))
+print(tup)
+
+'''Write a Python program to create a tuple with different data types.'''
+tup = ("tuple", False, 3.2, 1) #tuple created with different datatypes
+print(tup)
