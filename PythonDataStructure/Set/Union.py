87031961db0a7b3c11901e670fc229d30b458e53
naveen962 <75928781+naveen962@users.noreply.github.com>
2021-11-03 01:36:50 -0700

    Added: Python program to create a union of sets

---
+++ PythonDataStructure/Set/Union.py
@@ -0,0 +1,9 @@
+'''Write a Python program to create a union of sets.'''
+s1 = set(["green", "blue"])             #set created
+s2 = set(["blue", "yellow"])
+print("Original set elements:")
+print(s1)
+print(s2)
+print("\nUnion of two said sets:")
+s3 = s1 | s2                           #union of two sets
+print(s3)
