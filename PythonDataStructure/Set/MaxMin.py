7495652e7f0f756c3678686055cb99edb1217bc6
naveen962 <75928781+naveen962@users.noreply.github.com>
2021-11-03 01:33:33 -0700

    Added: Python program to find maximum and the minimum value in a set

---
+++ PythonDataStructure/Set/MaxMin.py
@@ -0,0 +1,5 @@
+''' Write a Python program to find maximum and the minimum value in a set.'''
+
+s=set([10,20,45,2,56,76])
+print(min(s)) #it gives minimum element in the set
+print(max(s)) #it gives maximum elementin the set
