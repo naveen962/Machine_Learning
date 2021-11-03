eb20023374943856b97b7bb926014ed7ee270440
naveen962 <75928781+naveen962@users.noreply.github.com>
2021-11-03 01:31:11 -0700

    Added: Python program to create an intersection of sets

---
+++ PythonDataStructure/Set/Intersection.py
@@ -0,0 +1,9 @@
+''' Write a Python program to create an intersection of sets.'''
+s1 = set(["green", "blue"])             #set created
+s2 = set(["blue", "yellow"])
+print("Original set elements:")
+print(s1)
+print(s2)
+print("\nIntersection of two said sets:")
+s3 = s1 & s2                           #intersection of two sets
+print(s3)
