c978907e2130c4e147e14e002046d32b3c200163
naveen962 <75928781+naveen962@users.noreply.github.com>
2021-11-03 01:34:29 -0700

    Added: Python program to remove item(s) from set

---
+++ PythonDataStructure/Set/RemoveElements.py
@@ -0,0 +1,14 @@
+'''  Write a Python program to remove item(s) from set.'''
+
+s={10,20,3.0,"apple",60} #set created 
+print(s)
+s.pop()
+print(s)
+
+
+'''Write a Python program to remove an item from a set if it is present in the set'''
+
+s={10,20,3.0,"apple",60} #set created 
+print(s)
+s.remove(60)
+print(s)
