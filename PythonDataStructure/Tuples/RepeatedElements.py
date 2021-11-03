06b3cefbcff4a79fe0f1128218cdbb33d3afd3ba
naveen962 <75928781+naveen962@users.noreply.github.com>
2021-11-02 21:41:31 -0700

    Added: program to find the repeated items of a tuple

---
+++ PythonDataStructure/Tuples/RepeatedElements.py
@@ -0,0 +1,6 @@
+'''Write a Python program to find the repeated items of a tuple.'''
+tup = (2, 4, 5, 6, 2, 3, 4, 4, 7 )
+print(tup)
+#return the number of times it appears in the tuple.
+count = tup.count(4)
+print(count)
