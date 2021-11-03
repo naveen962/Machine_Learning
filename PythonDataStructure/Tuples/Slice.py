3543fbfa0aff2d542f5cd10afde2811f9e9a9caf
naveen962 <75928781+naveen962@users.noreply.github.com>
2021-11-02 21:52:20 -0700

    Added: Program to slice a tuple

---
+++ PythonDataStructure/Tuples/Slice.py
@@ -0,0 +1,7 @@
+'''Write a Python program to slice a tuple.'''
+
+tup = (2, 4, 3, 5, 4, 6, 7, 8, 6, 1)
+#used tuple[start:stop] the start index is inclusive and the stop index
+slice = tup[3:5]
+#is exclusive
+print(slice)
