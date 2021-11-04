44486bac0847487ea3573c5d06fd0f0daaa2698a
naveen962 <75928781+naveen962@users.noreply.github.com>
2021-11-03 23:59:05 -0700

    Added: Python script that takes input from the user and displays that input back in upper and lower cases

---
+++ PythonDataStructure/Strings/UpperOrLower.py
@@ -0,0 +1,8 @@
+'''Write a Python script that takes input from the user and displays that input back in
+upper and lower cases.'''
+
+
+str=input("Enter the string : ") #taking user input
+print(str)
+print(str.upper())  #prints in uppercase
+print(str.lower()) #prints in lowercase
