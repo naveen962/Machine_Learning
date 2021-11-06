7566a0e80f3ff58d0adf1031676ff2da9641e181
naveen962 <75928781+naveen962@users.noreply.github.com>
2021-11-05 18:48:06 -0700

    Added: program to append a list to the second list

---
+++ PythonDataStructure/List/AppendList.py
@@ -0,0 +1,5 @@
+'''Write a Python program to append a list to the second list.'''
+list1 = [1, 2, 3, 0]
+list2 = ['Red', 'Green', 'Black']
+final_list = list1 + list2 #Appending List
+print(final_list)
