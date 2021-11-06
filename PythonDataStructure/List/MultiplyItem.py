10c276ae9a283a11dc6e21e76508e1d169f46b21
naveen962 <75928781+naveen962@users.noreply.github.com>
2021-11-05 19:28:21 -0700

    Added: program to multiplies all the items in a list

---
+++ PythonDataStructure/List/MultiplyItem.py
@@ -0,0 +1,7 @@
+'''Write a Python program to multiplies all the items in a list.'''
+
+list=[1,2,3,4,5] #list
+Multiply=1
+for x in list:
+    Multiply*=x
+print(Multiply) #multiplication of items in list
