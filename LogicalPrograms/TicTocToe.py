8abcc82e36ed8044998a373d92d8bdcd89827a4e
naveen962 <75928781+naveen962@users.noreply.github.com>
2021-11-05 20:57:46 -0700

    Refactor: program to play a Cross Game or Tic-Tac-Toe Game

--- LogicalPrograms/TicTocToe.py
+++ LogicalPrograms/TicTocToe.py
@@ -1,3 +1,9 @@
+'''Cross Game or Tic-Tac-Toe Game
+a. Desc -> Write a Program to play a Cross Game or Tic-Tac-Toe Game. Player 1
+is the Computer and the Player 2 is the user. Player 1 take Random Cell that is
+the Column and Row.'''
+
+
 import random
 print("Lets Play Tic-Toc-Toe Game")
 board = ["-", "-", "-",
@@ -5,7 +11,7 @@ board = ["-", "-", "-",
          "-", "-", "-"]
 
 
-game_still_going = True
+game_Running = True
 current_player = input("Who will start First X or O ? : ")
 
 #displaying board
@@ -22,7 +28,7 @@ def play():
     
        
     display()
-    while game_still_going:
+    while game_Running:
         handle_turn(current_player)
         gameover()
         flip_player()
@@ -37,12 +43,28 @@ def handle_turn(player):
         if current_player == "O":
             position = random.randint(0, 8)
         else:
-            position = int(input("Enter position from 1-9: ")) - 1
+            try:
+                position=pos()
+            except ValueError:
+                print("please enter postion between 1-9 ")
+                print("2 attempts Remaining ")
+                try:
+                    position=pos()
+                except ValueError:
+                    print("please enter postion between 1-9 ")
+                    print("1 attempts Remaining ")
+                    try:
+                        position=pos()
+                    except ValueError:
+                        print("please enter postion between 1-9 ")
+                        print("0 attempts Remaining ")
+                        quit()
+           
         while position not in [0, 1, 2, 3, 4, 5, 6, 7, 8]:
             if current_player == "O":
                 position = random.randint(0, 8)
             else:
-                position = int(input("Enter position from 1-9: ")) - 1
+                position = int(input("Enter position from 1 to 9: ")) - 1
         if board[position] == "-":
             valid = True
         else:
@@ -50,6 +72,15 @@ def handle_turn(player):
                 print("Choose differnt position")
     board[position] = player
     display()
+
+
+
+def pos():
+    position = int(input("Enter position from 1 to 9: ")) - 1
+    if position == 1 or 2 or 3 or 4 or 5 or 6 or 7 or 8 or 9:
+        return position
+
+    
 
 
 def flip_player():
@@ -69,7 +100,7 @@ def gameover():
 #checking  winner
 
 def winner():
-    global game_still_going
+    global game_Running
     row1 = board[0] == board[1] == board[2] != "-"
     row2 = board[3] == board[4] == board[5] != "-"
     row3 = board[6] == board[7] == board[8] != "-"
@@ -82,20 +113,20 @@ def winner():
     diagonal2 = board[2] == board[4] == board[6] != "-"
 
     if row1 or row2 or row3:
-        game_still_going = False
-        print(current_player, "won")
+        game_Running = False
+        print(current_player, "won the game")
     elif col1 or col2 or col3:
-        game_still_going = False
-        print(current_player, "won")
+        game_Running = False
+        print(current_player, "won the game")
     elif diagonal1 or diagonal2:
-        game_still_going = False
-        print(current_player, "won")
+        game_Running = False
+        print(current_player, "won the game")
 
 #Tie Game
 def tie():
-    global game_still_going
-    if ("-" not in board) and game_still_going:
-        game_still_going = False
+    global game_Running
+    if ("-" not in board) and game_Running:
+        game_Running = False
         print("Tie")
 
 
