1a537a6cb27824325278658a5d80dde1ac872ff5
naveen962 <75928781+naveen962@users.noreply.github.com>
2021-11-05 19:31:05 -0700

    Added: program to remove duplicates from a list

---
+++ PythonDataStructure/List/RemoveDuplicates.py
@@ -0,0 +1,10 @@
+'''Write a Python program to remove duplicates from a list'''
+
+a = [10,20,30,20,10,50,60,40,80,50,40]
+
+dup_items = set()
+for x in a:
+    if x not in dup_items: 
+        dup_items.add(x)
+
+print(dup_items)
