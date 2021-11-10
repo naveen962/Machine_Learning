9f52e1803da1d4c9a250740e32403cfab9f30acb
naveen962 <75928781+naveen962@users.noreply.github.com>
2021-11-10 03:58:41 -0800

    Added: program to create a 3-D array with ones on a diagonal and zeros

---
+++ Numpy/3DArray.py
@@ -0,0 +1,5 @@
+'''program to create a 3-D array with ones on a diagonal and zeros'''
+
+import numpy as np
+x = np.eye(3) #eye function is used to create multidimensional array with ones on the diagonal and zeros elsewhere
+print(x)
