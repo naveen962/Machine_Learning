e7641ac72b22a43e9d7bd1168399b32db60a823f
naveen962 <75928781+naveen962@users.noreply.github.com>
2021-11-10 05:38:09 -0800

    Added: program to iterate over rows in a DataFrame

---
+++ Pandas/Iterate.py
@@ -0,0 +1,7 @@
+'''program to iterate over rows in a DataFrame '''
+import pandas as pd
+import numpy as np
+exam_data = [{'name':'Anastasia', 'score':12.5}, {'name':'Dima','score':9}, {'name':'Katherine','score':16.5}]
+df = pd.DataFrame(exam_data)
+for index, row in df.iterrows():
+    print(row['name'], row['score'])
