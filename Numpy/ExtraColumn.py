c1091a5be7123cac0c948719fed5c2a16888e951
naveen962 <75928781+naveen962@users.noreply.github.com>
2021-11-10 04:59:35 -0800

    Added: NumPy program to add an extra column to a NumPy array

---
+++ Numpy/ExtraColumn.py
@@ -0,0 +1,6 @@
+'''NumPy program to add an extra column to a NumPy array'''
+
+import numpy as np
+x = np.array([[10,20,30], [40,50,60]])
+y = np.array([[100], [200]])
+print(np.append(x, y, axis=1))
