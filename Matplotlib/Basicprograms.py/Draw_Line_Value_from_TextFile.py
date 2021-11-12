d4562d8482be2e955dd149e2822859b514124612
naveen962 <75928781+naveen962@users.noreply.github.com>
2021-11-12 05:01:34 -0800

    Added: program to draw a line using given axis values taken from a text file

---
+++ Matplotlib/Basicprograms.py/Draw_Line_Value_from_TextFile.py
@@ -0,0 +1,18 @@
+''' Write a Python program to draw a line using given axis values taken from a text file, with
+suitable label in the x axis, y axis and a title.'''
+
+import matplotlib.pyplot as plt
+with open("test.txt") as f:
+    data = f.read()
+data = data.split('\n')
+x = [row.split(' ')[0] for row in data]
+y = [row.split(' ')[1] for row in data]
+plt.plot(x, y)
+# Set the x axis label of the current axis.
+plt.xlabel('x - axis')
+# Set the y axis label of the current axis.
+plt.ylabel('y - axis')
+# Set a title 
+plt.title('Sample graph!')
+# Display a figure.
+plt.show()
