3cf69e8295a364c8304ffbabcccbe2a1dce0d553
naveen962 <75928781+naveen962@users.noreply.github.com>
2021-11-12 05:05:19 -0800

    Added: draw a line using given axis with suitable values

---
+++ Matplotlib/Basicprograms.py/DrawLine_given_axis.py
@@ -0,0 +1,17 @@
+''' program to draw a line using given axis values with suitable label in the x axis , y axis and a title. '''
+
+import matplotlib.pyplot as plt
+# x axis values
+x = [1,2,3]
+# y axis values
+y = [2,4,1]
+# Plot lines and/or markers to the Axes.
+plt.plot(x, y)
+# Set the x axis label of the current axis.
+plt.xlabel('x - axis')
+# Set the y axis label of the current axis.
+plt.ylabel('y - axis')
+# Set a title 
+plt.title('Sample graph!')
+# Display a figure.
+plt.show()
